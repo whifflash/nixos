ba250f0129b26fdd5ab5da0a0b913bab4afd8f91
whifflash <solinus@protonmail.com>
2025-02-19 23:58:41 +0100

    ricing wofi

--- home/home.nix
+++ home/home.nix
@@ -71,6 +71,7 @@ in
       ".config/waybar/style.css".source = ./dotfiles/.config/waybar/style.css;
       ".config/waybar/config.jsonc".source = ./dotfiles/.config/waybar/config.jsonc; 
       ".config/wofi/style.css".source = ./dotfiles/.config/wofi/style.css; 
+      ".config/wofi/config".source = ./dotfiles/.config/wofi/config; 
       # ".config/gtk-3.0/gtk.css".source = ./dotfiles/.config/gtk-4.0/gtk.css; 
       # ".config/gtk-3.0/gtk.css".source = ./dotfiles/.config/gtk-3.0/gtk.css;
     };
