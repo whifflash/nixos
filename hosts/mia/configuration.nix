a387675abffb8fe327943258c5ad99f053bf3a50
whifflash <solinus@protonmail.com>
2025-03-08 19:00:25 +0100

    added networkmanaer configuration profiles

--- hosts/mia/configuration.nix
+++ hosts/mia/configuration.nix
@@ -23,6 +23,9 @@
   };
   sops.secrets."wireguard/vps/keys/public" = {
     owner = config.users.users."systemd-network".name;
+  };  
+  sops.secrets."network-manager.env" = {
+    owner = config.users.users."systemd-network".name;
   };
 
 
@@ -73,43 +76,43 @@
 
 
   # Enable WireGuard
-  networking.wireguard.enable = true;
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.40.80.2/24" ];
-      # listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+  # networking.wireguard.enable = true;
+  # networking.wireguard.interfaces = {
+  #   # "wg0" is the network interface name. You can name the interface arbitrarily.
+  #   wg0 = {
+  #     # Determines the IP address and subnet of the client's end of the tunnel interface.
+  #     ips = [ "10.40.80.2/24" ];
+  #     # listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = "${config.sops.secrets."wireguard/${specialArgs.hostname}/keys/private".path}";
+  #     # Path to the private key file.
+  #     #
+  #     # Note: The private key can also be included inline via the privateKey option,
+  #     # but this makes the private key world-readable; thus, using privateKeyFile is
+  #     # recommended.
+  #     privateKeyFile = "${config.sops.secrets."wireguard/${specialArgs.hostname}/keys/private".path}";
 
-      peers = [
-      # For a client configuration, one peer entry for the server will suffice.
+  #     peers = [
+  #     # For a client configuration, one peer entry for the server will suffice.
 
-      {
-        # Public key of the server (not a file path).
-        # publicKey = "$(cat ${config.sops.secrets."wireguard/vps/keys/public".path})";
-        publicKey = "EVYOMdJtLYAnx89F4GGZxDLy34DvIHCCty7gnxpM8yI=";
+  #     {
+  #       # Public key of the server (not a file path).
+  #       # publicKey = "$(cat ${config.sops.secrets."wireguard/vps/keys/public".path})";
+  #       publicKey = "EVYOMdJtLYAnx89F4GGZxDLy34DvIHCCty7gnxpM8yI=";
 
 
-        allowedIPs = [ "10.20.0.1/32" ];
-        # Or forward only particular subnets
-        #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+  #       allowedIPs = [ "10.20.0.1/32" ];
+  #       # Or forward only particular subnets
+  #       #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
-        # Set this to the server hostname and port. Using DNS should choose IPv4 or IPv6 depending on what works
-        endpoint = "vps.c4rb0n.cloud:51823"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+  #       # Set this to the server hostname and port. Using DNS should choose IPv4 or IPv6 depending on what works
+  #       endpoint = "vps.c4rb0n.cloud:51823"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
-        # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-        persistentKeepalive = 25;
-      }
-      ];
-    };
-  };
+  #       # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+  #       persistentKeepalive = 25;
+  #     }
+  #     ];
+  #   };
+  # };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
--- modules/roles/workstation.nix
+++ modules/roles/workstation.nix
@@ -49,6 +49,166 @@ in
       enableSSHSupport = true;
     };
 
+
+    systemd.services = {
+      NetworkManager-wait-online.enable = false;
+      NetworkManager-ensure-env-file = {
+        description = "Create NetworkManager env file if it does not exist";
+        wantedBy = [ "NetworkManager-ensure-profiles.service" ];
+        before = [ "NetworkManager-ensure-profiles.service" ];
+        serviceConfig.Type = "oneshot";
+        script = ''
+        if [ ! -d /etc/secrets ]; then
+        mkdir -p /etc/secrets
+        chmod 600 /etc/secrets
+        fi
+        if [ ! -f /etc/secrets/network-manager.env ]; then
+        touch /etc/secrets/network-manager.env
+        chmod 600 /etc/secrets/network-manager.env
+        fi
+        '';
+      };
+    };
+
+
+
+  networking = {
+    # firewall = {
+    #   logReversePathDrops = true;
+    #   # Do not block NetworkManager WireGuard via reverse path filter
+    #   # https://nixos.wiki/wiki/WireGuard
+    #   extraCommands = ''
+    #     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+    #     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+    #   '';
+    #   extraStopCommands = ''
+    #     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+    #     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+    #   '';
+    # };
+    # hostName = "mia";
+    networkmanager = {
+      enable = true;
+      ensureProfiles = {
+        environmentFiles = [
+          # "/run/secrets/network-manager.env"
+          # ${config.sops.secrets."network-manager.env".path}
+          # "/home/mhr/nm.env"
+          config.sops.secrets."network-manager.env".path
+          # "/etc/secrets/network-manager.env"
+        ];
+        profiles = {
+          "$LoT_SSID" = {
+            connection = {
+              id = "$LoT_SSID";
+              type = "wifi";
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              addr-gen-mode = "stable-privacy";
+              method = "auto";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$LoT_SSID";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$LoT_PSK";
+            };
+          };
+          # "$WORK_WG" = {
+          #   connection = {
+          #     autoconnect = false;
+          #     id = "$WORK_WG";
+          #     interface-name = "$WORK_WG";
+          #     type = "wireguard";
+          #     secondaries = "32986ce6-f9e4-37c8-96e1-baccfcc38f1a";
+          #   };
+          #   wireguard = {
+          #     mtu = 1280;
+          #     private-key = "$WORK_WG_PRIVATE_KEY";
+          #   };
+          #   "wireguard-peer.$WORK_WG_PUBLIC_KEY" = {
+          #     allowed-ips = "0.0.0.0/0;::/0;";
+          #     endpoint = "$WORK_WG:51820";
+          #     preshared-key = "$WORK_WG_PRESHARED_KEY";
+          #     preshared-key-flags = 0;
+          #   };
+          #   ipv4 = {
+          #     address1 = "$WORK_WG_IPV4_ADDR";
+          #     dns = "$WORK_WG_IPV4_DNS";
+          #     method = "manual";
+          #   };
+          #   ipv6 = {
+          #     address1 = "$WORK_WG_IPV6_ADDR";
+          #     dns = "$WORK_WG_IPV6_DNS";
+          #     method = "manual";
+          #   };
+          # };
+          # "$WORK_VPN" = {
+          #   connection = {
+          #     id = "$WORK_VPN";
+          #     type = "vpn";
+          #     uuid = "32986ce6-f9e4-37c8-96e1-baccfcc38f1a";
+          #   };
+          #   ipv4 = {
+          #     method = "auto";
+          #     never-default = "true";
+          #   };
+          #   ipv6 = {
+          #     addr-gen-mode = "stable-privacy";
+          #     method = "auto";
+          #     never-default = "true";
+          #   };
+          #   vpn = {
+          #     auth = "SHA512";
+          #     ca = "/etc/secrets/bundle.pem";
+          #     cert = "/etc/secrets/secret.p12";
+          #     cert-pass-flags = "0";
+          #     cipher = "AES-256-GCM";
+          #     connect-timeout = "1";
+          #     connection-type = "tls";
+          #     dev = "tun";
+          #     key = "/etc/secrets/secret.p12";
+          #     remote = "$WORK_CPN:1194:udp, $WOR_VPN:443:tcp";
+          #     remote-cert-tls = "server";
+          #     service-type = "org.freedesktop.NetworkManager.openvpn";
+          #     tls-crypt = "/etc/secrets/WORK_TLS_AUTH_SECRET_2024";
+          #   };
+          #   vpn-secrets = {
+          #     cert-pass = "$WORK_VPN_CERT_PW";
+          #   };
+          # };
+          # "$WORK_GUESTWIFI_SSID" = {
+          #   connection = {
+          #     id = "$WORK_GUESTWIFI_SSID";
+          #     type = "wifi";
+          #   };
+          #   ipv4 = {
+          #     method = "auto";
+          #   };
+          #   ipv6 = {
+          #     addr-gen-mode = "stable-privacy";
+          #     method = "auto";
+          #   };
+          #   wifi = {
+          #     mode = "infrastructure";
+          #     ssid = "$WORK_GUESTWIFI_SSID";
+          #   };
+          #   wifi-security = {
+          #     key-mgmt = "sae";
+          #     psk = "$WORK_GUESTWIFI_PSK";
+          #   };
+          # };
+        };
+      };
+    };
+  };
+
+
   };
 
 }
