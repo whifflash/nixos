12cde112693ddd4da610c59b93cf34eb2393a05e
whifflash <solinus@protonmail.com>
2025-04-12 07:58:54 +0200

    added more ssh stuff

--- home/home.nix
+++ home/home.nix
@@ -147,13 +147,34 @@ in
         };
 
         "wgbsw.vps.webdock.cloud" = {
-          # host = "vps";
           user = "mhr";
           hostname = "vps";
           identityFile = "/home/mhr/.ssh/webdockvps";
           identitiesOnly = true;
           port = 22;
         };
+        "git.c4rb0n.cloud" = {
+          user = "git";
+          hostname = "git.c4rb0n.cloud";
+          identityFile = "/home/mhr/.ssh/gitea";
+          identitiesOnly = true;
+          port = 2222;
+        };        
+        "ikarus" = {
+          user = "mhr";
+          hostname = "10.20.31.41";
+          identityFile = "/home/mhr/.ssh/gitea";
+          port = 22;
+        };       
+        "poseidon" = {
+          user = "mhr";
+          hostname = "127.0.0.1";
+          proxyJump = "vps";
+          dynamicForwards = [{port = 1080;}];
+        };
+
+
+
 
         # work = {
         #   host = (lib.concatStringsSep " " workHosts);
