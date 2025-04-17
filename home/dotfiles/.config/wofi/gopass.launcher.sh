cbcd3e348254f9c2af0fd60472321241d06ce55c
whifflash <solinus@protonmail.com>
2025-04-17 21:40:31 +0200

    fixed typo

--- home/dotfiles/.config/wofi/gopass.launcher.sh
+++ home/dotfiles/.config/wofi/gopass.launcher.sh
@@ -9,5 +9,5 @@ fi
 echo $PASSWORD_STORE_DIR
 
 PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR gopass ls --flat | \
-    wofi -dmenu -p "$PASSWORD_STORE_DIR" | \
+    wofi --dmenu --matching=fuzzy -p "$PASSWORD_STORE_DIR" | \
     xargs --no-run-if-empty -I {} /usr/bin/env bash -c 'PASSWORD_STORE_DIR='$PASSWORD_STORE_DIR' gopass show --clip "{}"'
