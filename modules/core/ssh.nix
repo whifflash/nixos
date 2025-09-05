7c3672638f308283f70c34f04b0d26ec702b9dd3
whifflash <34140499+whifflash@users.noreply.github.com>
2025-09-05 20:44:30 +0200

    added proper key handling for remote builder

--- modules/core/ssh.nix
+++ modules/core/ssh.nix
@@ -1,10 +1,10 @@
-_: {
+{inputs, ...}: {
   programs.ssh = {
     # enable = true;
 
     # System-wide known_hosts for the daemon & all users
     knownHosts = {
-      "icarus 10.20.31.41 attic.c4rb0n.cloud" = {
+      builder = {
         hostNames = ["10.20.31.41" "attic.c4rb0n.cloud"];
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjph8qyjvqVFPbuCOro4klZjO5X1HZxrWUe32Eja2RH";
       };
@@ -22,5 +22,16 @@ _: {
       #   publicKey = "ssh-ed25519 AAAA...your_gitea_hostkey...";
       # };
     };
+  };
+
+  # Provide a root-only SSH key via sops-nix (the private key)
+  sops.secrets."ssh/builder_ed25519" = {
+    sopsFile = inputs.self + /secrets/ssh.yaml;
+    format = "yaml";
+    key = "bsw/priv";
+    owner = "root";
+    group = "root";
+    mode = "0400";
+    path = "/root/.ssh/builder_ed25519";
   };
 }
