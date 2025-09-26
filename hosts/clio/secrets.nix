afab5fb24666d549239d0d386f57d9784146184a
mhr <mhr@c4rb0n.cloud>
2025-09-26 19:28:36 +0200

    maybe :D

--- hosts/clio/secrets.nix
+++ hosts/clio/secrets.nix
@@ -11,11 +11,13 @@
     sopsFile = ../../secrets/clio.yaml;
     # select the nested YAML key precisely:
     key = "[\"cloudflare\"][\"env\"]";
-    format = "dotenv";
+    owner = "root";
+    group = "root";
+    mode = "0400";
   };
 
   # optional: indicate where the age private key lives on the machine
-  age.keyFile = "/home/mhr/.config/sops/age/keys.txt";
+  age.keyFile = "/root/.config/sops/age/keys.txt";
 
     validateSopsFiles = true;
 
