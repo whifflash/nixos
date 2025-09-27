d3a374f72b3365679b4c1359764552273bb193ba
mhr <mhr@c4rb0n.cloud>
2025-09-27 08:27:20 +0200

    new try

--- hosts/clio/secrets.nix
+++ hosts/clio/secrets.nix
@@ -1,22 +1,25 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
+  imports = [ inputs.sops-nix.nixosModules.sops ];
   # All clio secrets live here
   sops = {
     defaultSopsFile = ../../secrets/clio.yaml;
 
   secrets."cloudflare/env" = {
-    sopsFile = ../../secrets/clio.yaml;
-    # select the nested YAML key precisely:
-    key = "[\"cloudflare\"][\"env\"]";
+    format = "dotenv";
+    # If the consumer is a non-root service, set owner/group to that user.
     owner = "root";
     group = "root";
-    mode = "0400";
+    mode  = "0400";
+    # If a service reads it, add it here to restart on change, e.g.:
+    # restartUnits = [ "caddy.service" ];
   };
 
-  age.keyFile = "/home/mhr/.config/sops/age/keys.txt";
+  age.keyFile = "/var/lib/sops-nix/key.txt";
 
   validateSopsFiles = true;
 
