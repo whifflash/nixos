f3a6c8587b5e89c30c45bb6b30cb81822daf0b7f
mhr <mhr@c4rb0n.cloud>
2025-09-28 08:37:03 +0200

    debugging clio

--- hosts/clio/storage.nix
+++ hosts/clio/storage.nix
@@ -8,8 +8,6 @@ in {
   options.clio.enableDisko = mkEnableOption "Enable Disko-managed disks for Clio";
 
   config = mkIf config.clio.enableDisko {
-    # NOTE: disko module is already imported via default.nix
-    # Define your disk layout here. (singular 'disk' is correct)
     disko.devices = {
       disk.main = {
         device = "/dev/vda"; # change to your real disk on bare metal
