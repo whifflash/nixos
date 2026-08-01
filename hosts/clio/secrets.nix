302a96df8fa8d1a1324579fc12b0dee5c09297a9
mhr <mhr@c4rb0n.cloud>
2026-08-01 11:34:17 +0200

    removed old hosts

--- hosts/clio/secrets.nix
+++
@@ -1,8 +0,0 @@
-_: {
-  # Host-specific decryption key location. Secret declarations live with the
-  # shared service modules that consume them.
-  sops = {
-    age.keyFile = "/root/.config/sops/age/keys.txt";
-    validateSopsFiles = false;
-  };
-}
